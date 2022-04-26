2e1c92f676645e2e2319f350a1e7d252281b2bac
Ziad Moftah <43881897+ziadmoftah@users.noreply.github.com>
2022-04-26 02:42:11 +0200

    Implement Authentication Component
    HTML, CSS and module were implemented

--- Coursero/src/app/authentication/authentication.component.ts
+++ Coursero/src/app/authentication/authentication.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-authentication',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthenticationComponent implements OnInit {
 
-  constructor() { }
+  Users:UserComponent[] ;
+  errorMessage:string = "" ;
+  constructor() { 
+    this.Users = [] ; 
+  }
 
   ngOnInit(): void {
   }
+  Register(name:string , account:string , password:string): Boolean {
+    if ( name == ""|| account == "" || password == ""){   // Whether any text field is empty
+      this.errorMessage = "Please Make Sure You Have Filled All The Fields" ;
+      return false;
+    }
 
+    for (let i = 0; i < this.Users.length; i++) {  // Check if same Account already exists
+      let currenUser:UserComponent = this.Users[i] ;
+      if (currenUser.Get_Account() == account){
+        this.errorMessage = "Account Already Used" ; 
+        return false; 
+      }
+    }
+
+    let newUser:UserComponent = new UserComponent() ;
+    newUser.FillData(name , account , password ) ;
+    this.Users.push(newUser);
+    this.errorMessage = "User Registered Successfully" ;
+    return true ; 
+  }
+
+  Login(account:string , password:string):Boolean {
+    if (  account == "" || password == ""){   // Whether any text field is empty
+      this.errorMessage = "Please Make Sure You Have Filled All The Fields" ;
+      return false;
+    }
+    let isfound:boolean = false;
+    for (let i = 0; i < this.Users.length; i++) {
+      let currenUser:UserComponent = this.Users[i] ;
+      if ( currenUser.Get_Account() == account ){
+        isfound = true ; 
+      }
+      if (currenUser.Get_Account() == account && currenUser.Get_Password() != password){
+        this.errorMessage = "Password Incorrect" ;
+        return false;
+      }
+      if (currenUser.Get_Account() == account && currenUser.Get_Password() == password){
+        this.errorMessage = "Valid User" ;
+        return true;
+      }
+      
+    }
+    if (isfound == false ){
+      this.errorMessage = "Invalid User" ; 
+      return false;
+    }
+    this.errorMessage = "User Invalid" ; 
+    return false ;
+  }
 }
