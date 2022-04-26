883133d4099d59a9c557ea91de1607b71adc2394
Ziad Moftah <43881897+ziadmoftah@users.noreply.github.com>
2022-04-26 02:39:37 +0200

    Generate Authentication Component

--- Coursero/src/app/app.module.ts
+++ Coursero/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule
