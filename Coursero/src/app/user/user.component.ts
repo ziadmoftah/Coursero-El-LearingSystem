d7924d6cb694124fb50cf3e8b7ca6650f48c4ba2
Ziad Moftah <43881897+ziadmoftah@users.noreply.github.com>
2022-04-26 02:38:57 +0200

    Implement User Component

--- Coursero/src/app/user/user.component.ts
+++ Coursero/src/app/user/user.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  name:string ;
+  account:string;
+  password:string;
+  constructor() { 
+    this.name = this.password = this.account = "" ; 
+  }
 
   ngOnInit(): void {
   }
 
+  FillData(name:string , account:string , password:string){
+    this.name = name ; 
+    this.account = account ; 
+    this.password = password ; 
+  }
+
+  Get_Name():string{
+    return this.name;
+  }
+  Get_Account():string{
+    return this.account;
+  }
+  Get_Password():string{
+    return this.password;
+  }
 }
